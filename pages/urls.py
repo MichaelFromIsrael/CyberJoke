42819429c955dd8f01c670082383e329b1353015
Michael <michaelbehar99@gmail.com>
2020-05-03 15:05:28 +0300

    Second

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from .views import HomePageView
-
+from .views import HomePageView, SignUp
+from django.contrib import admin
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', SignUp.as_view(), name='signup'),
     
     
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
